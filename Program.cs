ef4ff6da92522c7ce001d12bdaab0624c2261101
Rafael Delgado <rafael@delgado.com>
2022-05-20 17:48:49 -0700

    update job to run

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No argument.");
+            return;
+        }
+
+        var filePath = Path.GetFullPath(Path.Combine(".", args[0]));
+        if (!File.Exists( filePath))
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        var fileContent = File.ReadAllText(filePath);
+        Console.WriteLine(fileContent);
+    }
+}
